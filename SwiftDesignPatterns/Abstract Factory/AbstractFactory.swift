dc47144735cfe546e06dbd7314ac84434baa8001
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-16 22:24:37 +0900

    AbstractFactoryのファイルを追加

---
+++ SwiftDesignPatterns/Abstract Factory/AbstractFactory.swift
@@ -0,0 +1,15 @@
+//
+//  AbstractFactory.swift
+//  SwiftDesignPatterns
+//
+//  Created by jun on 2020/10/16.
+//
+
+import Foundation
+
+
+class AbstractFactory {
+    static func main() {
+        
+    }
+}
