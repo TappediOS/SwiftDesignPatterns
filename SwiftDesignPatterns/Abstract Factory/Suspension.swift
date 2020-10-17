c1adafb57f8399dc3529bef82a08fe4525b738a2
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-17 17:00:17 +0900

    Suspensionファイルの中身を記述

--- SwiftDesignPatterns/Abstract Factory/Suspension.swift
+++ SwiftDesignPatterns/Abstract Factory/Suspension.swift
@@ -5,4 +5,24 @@
 //  Created by jun on 2020/10/17.
 //
 
-import Foundation
+protocol Suspension {
+    var type: SuspensionOption { get }
+}
+
+enum SuspensionOption: String {
+    case standard = "standard"
+    case sport = "sport"
+    case soft = "soft"
+}
+
+class RoadSus: Suspension {
+    var type: SuspensionOption = .standard
+}
+
+class OffRoeaSus: Suspension {
+    var type: SuspensionOption = .soft
+}
+
+class RaceSus: Suspension {
+    var type: SuspensionOption = .sport
+}
