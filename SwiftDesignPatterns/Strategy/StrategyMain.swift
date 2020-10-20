12b04b5de33b8636acbd7f1e8292b0206c096a44
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-10-20 22:35:16 +0900

    StrategyMainを追加

---
+++ SwiftDesignPatterns/Strategy/StrategyMain.swift
@@ -0,0 +1,13 @@
+//
+//  StrategyMain.swift
+//  SwiftDesignPatterns
+//
+//  Created by jun on 2020/10/20.
+//
+
+class StrategyMain {
+    static func main() -> String {
+        let seq = Sequence([1, 2, 3, 4])
+        seq.addNum
+    }
+}
